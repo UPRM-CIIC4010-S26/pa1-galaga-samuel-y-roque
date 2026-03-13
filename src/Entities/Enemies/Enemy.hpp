f963e5d6559f3f3155803c489b5a00fa2a42714e
Samuel-Ruiz7 <samuel.ruiz7@upr.edu>
2026-03-13 19:29:29 -0400

    cambios a enemy correctos

--- src/Entities/Enemies/Enemy.hpp
+++ src/Entities/Enemies/Enemy.hpp
@@ -4,6 +4,7 @@
 #include "ImageManager.hpp"
 #include "Animation.hpp"
 #include <iostream>
+#include "SoundManager.hpp"
 
 class Enemy {
     protected:
@@ -24,6 +25,7 @@ class Enemy {
         inline static float direction = 0.5;
         inline static int directionChange = 100;
         inline static std::vector<std::pair<std::pair<float, float>, Enemy*>> enemies;
+        inline static int score = 0;
 
         Enemy() {}
 
@@ -55,12 +57,16 @@ class Enemy {
                     for (Projectile& p2 : Projectile::projectiles) {
                         if (p2.ID != 1 && HitBox::Collision(p.second->hitBox, p2.getHitBox())) {
                             p.second->health--;
+                            if (p.second->health > 0) {
+                            PlaySound(SoundManager::hit);
+                                }
                             p2.del = true;
                         }
                     }
 
                     if (p.second->health <= 0) {
-                        //score += p.second->points; //score acordado por partner
+                        PlaySound(SoundManager::dead);
+                        Enemy::score += p.second->points; //score acordado por partner
                         Animation::animations.push_back(
                             Animation(p.second->position.first, p.second->position.second, 155, 0, 33, 33, 30, 30, 4, ImageManager::SpriteSheet)
                         );
