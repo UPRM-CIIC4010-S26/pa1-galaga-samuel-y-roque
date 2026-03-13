2c5c5b07b140e31af56d2d034ac653feb06f0656
Samuel-Ruiz7 <samuel.ruiz7@upr.edu>
2026-03-13 19:14:30 -0400

    commit de enemy pt2

--- src/Entities/Enemies/Enemy.hpp
+++ src/Entities/Enemies/Enemy.hpp
@@ -60,7 +60,7 @@ class Enemy {
                     }
 
                     if (p.second->health <= 0) {
-                        score += p.second->points; //score acordado por partner
+                        //score += p.second->points; //score acordado por partner
                         Animation::animations.push_back(
                             Animation(p.second->position.first, p.second->position.second, 155, 0, 33, 33, 30, 30, 4, ImageManager::SpriteSheet)
                         );
